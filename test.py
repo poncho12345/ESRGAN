06ec57c3d2196a8b21431361a7336b604f20f0fd
poncho <81554647+poncho12345@users.noreply.github.com>
2021-03-29 02:13:21 -0600

    Update test.py

--- test.py
+++ test.py
@@ -5,16 +5,26 @@ import numpy as np
 import torch
 import RRDBNet_arch as arch
 
+<<<<<<< HEAD
 model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
+=======
+model_path = sys.argv[1]  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
+>>>>>>> parent of 6f106de (add run on CPU)
 
 test_img_folder = 'LR/*'
 
 model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 model.load_state_dict(torch.load(model_path), strict=True)
 model.eval()
+<<<<<<< HEAD
 model = model.to(device)
+=======
+for k, v in model.named_parameters():
+    v.requires_grad = False
+model = model.cuda()
+>>>>>>> parent of 6f106de (add run on CPU)
 
 print('Model path {:s}. \nTesting...'.format(model_path))
 
@@ -28,7 +38,7 @@ for path in glob.glob(test_img_folder):
     img = img * 1.0 / 255
     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
     img_LR = img.unsqueeze(0)
-    img_LR = img_LR.to(device)
+    img_LR = img_LR.cuda()
 
     with torch.no_grad():
         output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
